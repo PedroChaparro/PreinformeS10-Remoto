4db324977146dfdfaa004ea179ff9175b78ac72f
Pedro Andrés Chaparro Quintero <pedro.chaparro.2020@upb.edu.co>
2020-04-18 16:01:38 -0500

    Solución Parte A - Literal C

--- informe11_A_PedroChaparro.py
+++ informe11_A_PedroChaparro.py
@@ -12,3 +12,21 @@ def generador(min, max):
 
 ingresos = generador(100, 181)
 egresos = generador(60, 130)
+
+#Numeral c 
+
+def imprimir(bidim):
+    
+    print("Filas --> {} <-- Columnas".format(bidim.shape))
+    
+    ciudades = np.array(["Bucaramanga", "Floridablanca", "Girón", "Piedecuesta"])
+    meses = np.array(["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiempre", "Octubre", "Nobiembre", "Diciembre"])
+    
+    for c in range(0,4,1):
+        print("\n")
+        print("Para la ciudad: {}, los ingresos, o egresos, según el caso, en millones de pesos fueron: ".format(ciudades[c]))
+        for f in range(0,12, 1):
+            print("Para el mes {}: {}".format(meses[f] , bidim[c][f]))
+            
+llamar = imprimir(ingresos)
+            
